015a5c3f2136c8a96f2cf3c1ffc0112895ee9cbf
Alexandra Messner <54995607+EgoDaemon@users.noreply.github.com>
2020-08-02 14:10:26 +0300

    update

--- As_Dashboard.sql
+++ As_Dashboard.sql
@@ -18,3 +18,41 @@ from public.orders o
 group by state
 order by state asc;
 
+UPDATE orders
+SET Person = (SELECT Person FROM people WHERE Region = 'West')
+WHERE Region = 'West';
+UPDATE orders
+SET Person = (SELECT Person FROM people WHERE Region = 'East')
+WHERE Region = 'East';
+UPDATE orders
+SET Person = (SELECT Person FROM people WHERE Region = 'Central')
+WHERE Region = 'Central';
+UPDATE orders
+SET Person = (SELECT Person FROM people WHERE Region = 'South')
+WHERE Region = 'South';
+
+SELECT * FROM orders;
+
+SELECT count(returned) FROM orders left Join returns USING(Order_id)
+where returned is not Null
+;
+
+SELECT person as Менеджер, count(returned) as Количество_возвратов FROM orders left Join returns USING(Order_id)
+where returned is not null
+group by person
+;
+
+SELECT state, count(returned) as Количество_возвратов FROM orders left Join returns USING(Order_id)
+where returned is not null
+group by state
+;
+
+SELECT count(returned) as Количество_возвратов FROM orders left Join returns USING(Order_id)
+where returned is not null;
+
+select count(distinct (Order_id)) from orders left Join returns USING(Order_id)
+where returned is not null;
+
+select person, round((avg(Discount)*100), 2)  from orders o
+where Discount <> 0
+group by person;
